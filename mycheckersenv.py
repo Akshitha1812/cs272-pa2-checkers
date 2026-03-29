86173eec5ac89f42de9ed605703634f560715799
Akshitha1812 <akshitha@example.com>
2026-03-29 15:25:18 -0700

    Fix mandatory jump win-check bug and increase training

--- mycheckersenv.py
+++ mycheckersenv.py
@@ -127,9 +127,11 @@ class raw_env(AECEnv):
         forward_dir = 1 if is_p1 else -1
         
         for i in range(36):
-            if self.mandatory_jumper is not None and self.board[self.mandatory_jumper] in pieces:
-                if i != self.mandatory_jumper:
-                    continue
+            # Only restrict to mandatory jumper if the jumper belongs to the current agent
+            if self.mandatory_jumper is not None:
+                if self.board[self.mandatory_jumper] in pieces:
+                    if i != self.mandatory_jumper:
+                        continue
                 
             if self.board[i] in pieces:
                 r, c = i // 6, i % 6
